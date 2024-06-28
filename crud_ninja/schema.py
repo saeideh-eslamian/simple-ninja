06e3d6e0c2e7a73ef4db4d1d5a588d608cce2059
saeideh-eslamian <s.eslamian.70@gmail.com>
2024-06-28 15:15:23 +0200

    feat: add filter to students endpoint

--- crud_ninja/schema.py
+++ crud_ninja/schema.py
@@ -1,22 +1,15 @@
-from ninja import Schema
+from ninja import Schema, FilterSchema, Field
 from pydantic import HttpUrl
+from typing import Optional
 
 
-
-class StudentSchema(Schema):
-    id: int
-    first_name: str
-    last_name: str
-    image: HttpUrl
-    age: int
-    grade: str
 
 
 class TeacherSchema(Schema):
     id: int
     first_name: str
     last_name: str
-    image: HttpUrl
+    # image: HttpUrl
     teaching: str
 
 
@@ -26,3 +19,24 @@ class SchoolSchema(Schema):
     city: str
     level: str
     kind: str
+    teachers: list[TeacherSchema]
+
+
+
+class StudentSchema(Schema):
+    id: int
+    first_name: str
+    last_name: str
+    # image: HttpUrl
+    age: int
+    grade: str
+    teachers: list[TeacherSchema]
+    school : SchoolSchema
+
+class StudentFilterSchema(FilterSchema):
+    age: Optional[int] = Field(None)
+    teachers: Optional[list] = Field(None, q=[
+        'teachers__first_name__icontains',
+        'teachers__last_name__icontains'
+        ])
+    school: Optional[str] = Field(None, q='school__name__icontains')
